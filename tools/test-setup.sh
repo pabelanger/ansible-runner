0ec0d866b3e0cdb03181275dc9cab10084051a34
Paul Belanger <pabelanger@redhat.com>
2019-09-19 16:12:14 -0400

    WIP: Do not merge
    Testing fedora-30 images to ensure nothing breaks.
    
    Depends-On: https://github.com/ansible/ansible-zuul-jobs/pull/213
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -4,4 +4,4 @@
 # to the latest version
 # NOTE(pabelanger): Cap zipp<0.6.0 due to python2.7 issue with more-iterrtools
 # https://github.com/jaraco/zipp/issues/14
-sudo pip install -U tox "zipp<0.6.0"
+sudo pip3 install -U tox "zipp<0.6.0"
